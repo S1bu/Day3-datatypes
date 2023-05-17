0c14141d3b07ff24944a3351573bf00c0fa2024b
S1bu <Sibusisodyan@gmail.com>
2023-05-17 09:55:00 +0200

    datatype

---
+++ script/code.js
@@ -0,0 +1,48 @@
+// 1.
+let name = 10 ;
+
+// 1.
+// console.log(typeof name)
+
+//2.yes it changed into number
+
+/*
+string
+number
+boolean
+undefined
+NULL
+object
+function
+array
+*/
+
+ //string
+let string = 'Sibusiso';
+ //number
+let number = 123456789;
+//boolean
+let boolean = true; 
+//NULL
+let nullVariable = null;
+nullVariable = 'string';
+nullVariable = null;
+//undefined
+let undefinedVariable;
+// object
+let object={
+    name:'Sibusiso',
+}
+//function
+function fun() {
+}
+
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof boolean);
+console.log(typeof nullVariable);
+console.log(typeof undefinedVariable);
+console.log(typeof object);
+console.log(typeof fun);
+
